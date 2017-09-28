bd9019450500fe575a1406015ca506f12f613608
rodhall85 <rodhall@hotmail.co.uk>
2017-09-28 23:29:25 +0100

    Add sync-t-s3.sh script with basic functionality

---
+++ sync-to-s3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find . -name "*.mp4" -exec mv -t /tmp/zm {} +
+
+zip `$(date +"%Y-%m-%dT%H:%M:%S)`.zip /tmp/zm/
+
+rm -f /tmp/zm/*.mp4
+
+aws s3 mv ~/zoneminder/ s3://zoneminder-events --recursive
